a1e26c114892a592745b8070b616888a38d3607e
kupszi <f.farkas.erik998gmail.com>
2023-12-10 20:04:00 +0100

    Auth Level Middleware implemented

---
+++ src/Middleware/AuthLevelMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace szolprog\API\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Psr7\Response;
+use szolprog\API\DB;
+class AuthLevelMiddleware {
+    public function __invoke(Request $request, RequestHandler $handler){
+
+        $token = $request->getHeaderLine('Token');
+        if ($token) {
+            $db = new DB();
+            $pdo = $db->connect();
+            $statement = $pdo->prepare('SELECT is_admin FROM user WHERE token = ?');
+            $statement->execute([$token]);
+            if ($statement->fetch(\PDO::FETCH_ASSOC)['is_admin'] == 1) {
+                return $handler->handle($request);
+            }
+        }
+
+        $data = ['error' => "Authorization level error. You don't have permission to use this route"];
+        $response = new Response();
+        $response->getBody()->write(json_encode($data));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(401);
+    }
+
+}
